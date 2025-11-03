ff1a8ffd6a2f2e921aee02afa7e21054bb5a9c06
Rushikesh-9124 <rushikesh6281@gmail.com>
2025-11-03 11:13:01 +0530

    Update axiosInstance.js

--- frontend/src/helper/axiosInstance.js
+++ frontend/src/helper/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/api/events',
+    baseURL: 'https://google-calendar-clone-3.onrender.com/api/events',
     timeout: 60000,
     headers: {
         "Content-Type": "application/json"
